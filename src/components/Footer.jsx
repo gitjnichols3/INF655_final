526cfd3485d6832e700b4f11a87a41e470efc72e
gitjnichols3 <j_nichols2@mail.fhsu.edu>
2026-05-02 18:01:02 -0700

    Added a discalimer to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,13 @@ function Footer() {
       <p>Share the Moment</p>
       <p>Jim Nichols | INF655G Final Project</p>
       <p>Fort Hays University | Spring 2026</p>
+        <p className="footer-disclaimer">
+    This is a student project for educational purposes only. 
+    Content may be modified or removed at any time and should not be considered permanent. 
+    Users are responsible for any images or material they upload. 
+    Please do not upload sensitive, private, or copyrighted content. 
+    The creator is not responsible for user-submitted content.
+  </p>
     </footer>
   );
 }
